be1311bfa2946240edf1c3672ebee37ea1e81856
Joaquin Florius <j@florius.com.ar>
2020-10-02 21:01:07 +0200

    Initial commit

---
+++ src/main/kotlin/ar/com/florius/Hello.kt
@@ -0,0 +1,6 @@
+package ar.com.florius
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/ar/com/florius/HelloTest.kt
@@ -0,0 +1,8 @@
+package ar.com.florius
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
